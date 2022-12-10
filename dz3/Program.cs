3defc9fd3f9e34a58958684766d487653eee7468
SvetlanaMedvedeva73 <sweta.medvedev16@yandex.ru>
2022-12-10 18:35:32 +0300

    dz3

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+
+int n;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+for (int i = 1; i <= n; i++)
+Console.Write($"{i * i * i}");
